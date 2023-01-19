d78f42ed513e87aee7b4ed1302604f94d5f3ab7b
Gennadiy Ulyanov <ermakoventer777@gmail.com>
2023-01-19 13:10:55 +0300

    Test

--- application.py
+++ application.py
@@ -6,6 +6,7 @@
 # 1.0          13-Dec-2019    Initial Version
 #
 # ----------------------------------------------
+#privet
 from flask import Flask, render_template
 
 application = Flask(__name__)
